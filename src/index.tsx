0b8bac2410e6ac677df89e208d874c54df444093
jenson <jenson.sun@msn.com>
2021-12-06 00:29:09 +0800

    style
    style
    
    up
    
    background
    
    style
    
    up

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { Global, css } from '@emotion/react'
+import './utils/rem'
 
 import reportWebVitals from './reportWebVitals'
 
@@ -17,6 +18,17 @@ const globalStyle = css`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-transform: uppercase;
+    background: #000;
+    user-select: none;
+  }
+
+  img {
+    display: block;
+  }
+
+  .swiper-container {
+    width: 100%;
+    height: 100%;
   }
 `
 
