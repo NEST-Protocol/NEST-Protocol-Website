8cc4207ea5991d06568c1493fc4a6d1561b169a1
tunogya <tunogya@qq.com>
2022-07-20 21:18:56 +0800

    update

--- src/pages/Community/index.tsx
+++ src/pages/Community/index.tsx
@@ -6,7 +6,7 @@ import {
   chakra,
   useMediaQuery,
   Text,
-  Stack, IconButton
+  Stack, IconButton, Wrap
 } from '@chakra-ui/react'
 import Right from '../../assets/svg/right_icon.svg'
 import * as React from 'react'
@@ -42,7 +42,7 @@ const Community = () => {
       bg: Discord_pic,
       icon: Discord_icon,
       link: 'https://nest-protocol-82041.medium.com/'
-    }
+    },
   ]
 
   return (
@@ -93,22 +93,21 @@ const Community = () => {
           community and get reward.
         </Text>
       </Stack>
-      <Stack align={'center'}>
-        <Stack
-          px={['24px', '48px']}
-          align={'center'}
-          minH={'440px'}
-          justify={'center'}
-          spacing={'16px'}
-          w={'full'}
-        >
-          <SimpleGrid columns={[1, 3]} spacing={['22px', '22px', '44px']}>
+
+      { isDesktop ? (
+        <Stack align={'center'}>
+          <Wrap
+            px={['24px', '48px']}
+            align={'center'}
+            justify={'center'}
+            spacing={['24px', '48px']}
+          >
             {menu.map(item => (
               <VStack
                 key={item.label}
                 borderRadius={'12px'}
                 spacing={0}
-                w={['full', 'full', '308px']}
+                w={['40%', '308px']}
                 border={'1px solid #EEEEEE'}
                 boxShadow={'0px 0px 45px 5px #E5E5E5'}
                 overflow={'hidden'}
@@ -120,43 +119,121 @@ const Community = () => {
                   align={'center'}
                   justify={'center'}
                   h={'200px'}
-                  w={'100vw'}
+                  w={"full"}
                 >
                   <chakra.img src={item.icon} _hover={{transform: 'scale(1.05)'}}/>
                 </Stack>
                 <HStack
-                  px={['22px', '22px', '44px']}
-                  py={['11px', '11px', '33px']}
+                  px={'44px'}
+                  py={'33px'}
                   justifyContent={'space-between'}
                   w={'full'}
                   alignItems={'center'}
                 >
-                  <Text fontSize={['12px', '12px', '17px']} fontWeight={'semibold'} color={'#003232'}>
+                  <Text fontSize={'17px'} fontWeight={'semibold'} color={'#003232'}>
                     {item.label}
                   </Text>
                   <Link isExternal href={item.link}>
-                    {isDesktop ? (
-                      <IconButton
-                        aria-label={''}
-                        _hover={{transform: 'translateX(8px)'}}
-                        icon={<chakra.img
-                          src={Right}
-                          alt={'right'}
-                          w={'44px'}
-                        />} size={'24px'}/>
-                    ) : (
-                      <IconButton
-                        aria-label={''}
-                        icon={ <chakra.img src={Right} alt={'right'} height={'24px'} width={'24px'}/>}
-                      />
-                    )}
+                    <IconButton
+                      aria-label={''}
+                      _hover={{transform: 'translateX(8px)'}}
+                      icon={<chakra.img src={Right} alt={'right'} w={'44px'}/>} size={'24px'}/>
                   </Link>
                 </HStack>
               </VStack>
             ))}
-          </SimpleGrid>
+          </Wrap>
         </Stack>
-      </Stack>
+      ) : (
+        <Stack px={'24px'} spacing={'24px'}>
+          <HStack spacing={'24px'}>
+            {menu.slice(0, 2).map(item => (
+              <VStack
+                key={item.label}
+                borderRadius={'12px'}
+                spacing={0}
+                w={'full'}
+                border={'1px solid #EEEEEE'}
+                boxShadow={'0px 0px 45px 5px #E5E5E5'}
+                overflow={'hidden'}
+              >
+                <Stack
+                  bgImage={item.bg}
+                  bgRepeat={'no-repeat'}
+                  bgSize={'cover'}
+                  align={'center'}
+                  justify={'center'}
+                  h={'100px'}
+                  w={'full'}
+                >
+                  <chakra.img
+                    src={item.icon}
+                    _hover={{transform: 'scale(1.05)'}}
+                    h={'65px'}
+                  />
+                </Stack>
+                <HStack
+                  px={'11px'}
+                  py={'11px'}
+                  justifyContent={'space-between'}
+                  w={'full'}
+                  alignItems={'center'}
+                >
+                  <Text fontSize={'12px'} fontWeight={'semibold'} color={'#003232'}>
+                    {item.label}
+                  </Text>
+                  <Link isExternal href={item.link}>
+                    <chakra.img src={Right} alt={'right'} minW={'22px'} minH={'22px'}/>
+                  </Link>
+                </HStack>
+              </VStack>
+            ))}
+          </HStack>
+          <HStack justifyContent={"center"}>
+            {menu.slice(2, 3).map(item => (
+              <VStack
+                key={item.label}
+                borderRadius={'12px'}
+                spacing={0}
+                w={'47%'}
+                border={'1px solid #EEEEEE'}
+                boxShadow={'0px 0px 45px 5px #E5E5E5'}
+                overflow={'hidden'}
+              >
+                <Stack
+                  bgImage={item.bg}
+                  bgRepeat={'no-repeat'}
+                  bgSize={'cover'}
+                  align={'center'}
+                  justify={'center'}
+                  h={['100px', '120px']}
+                  w={'full'}
+                >
+                  <chakra.img
+                    src={item.icon}
+                    _hover={{transform: 'scale(1.05)'}}
+                    h={'65px'}
+                  />
+                </Stack>
+                <HStack
+                  px={'11px'}
+                  py={'11px'}
+                  justifyContent={'space-between'}
+                  w={'full'}
+                  alignItems={'center'}
+                >
+                  <Text fontSize={'12px'} fontWeight={'semibold'} color={'#003232'}>
+                    {item.label}
+                  </Text>
+                  <Link isExternal href={item.link}>
+                    <chakra.img src={Right} alt={'right'} minH={'22px'} minW={'22px'}/>
+                  </Link>
+                </HStack>
+              </VStack>
+            ))}
+          </HStack>
+        </Stack>
+      ) }
     </>
   )
 }
