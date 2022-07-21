2ef156551b5598a3b232b25502128dc07d84da1d
tunogya <tunogya@qq.com>
2022-07-21 14:25:35 +0800

    update

--- src/components/AlertFromFORT/index.tsx
+++ src/components/AlertFromFORT/index.tsx
@@ -4,13 +4,14 @@ import {
   AlertDialog,
   AlertDialogBody,
   AlertDialogCloseButton,
-  AlertDialogContent,
+  AlertDialogContent, AlertDialogHeader,
   AlertDialogOverlay, chakra, HStack, Stack, Text, useDisclosure
 } from "@chakra-ui/react";
 import NEST_URI from "../../assets/svg/nest.svg";
 import FORT_URI from "../../assets/svg/fort.svg";
 import * as React from "react";
 import {SmallCloseIcon} from "@chakra-ui/icons";
+import Banner_URI from "../../assets/webp/Ecosystem_pic_1.webp";
 
 const AlertFromFORT = () => {
   let [searchParams] = useSearchParams();
@@ -33,38 +34,50 @@ const AlertFromFORT = () => {
       isCentered
       autoFocus={false}
       closeOnOverlayClick={false}
+      size={'lg'}
     >
       <AlertDialogOverlay/>
-      <AlertDialogContent p={'24px'}>
-        <AlertDialogCloseButton/>
-        <AlertDialogBody>
-          <Stack fontSize={'md'} color={'#003434'} fontWeight={'500'} spacing={'24px'}>
-            <HStack justify={"center"}>
-              <chakra.img src={NEST_URI}/>
-              <SmallCloseIcon />
-              <chakra.img src={FORT_URI}/>
-            </HStack>
-            <Text>
-              The technical teams of the FORT protocol and NEST
-              protocol will jointly advance the merger, and the merger is
-              expected to be completed on July 29th.
-            </Text>
-            <Text>
-              Starting from 10:00 (GMT+0) July 7 (some suspensions will
-              take place a few hours earlier due to technical reason).
-            </Text>
-            <Text>
-              1. the users will be suspended from buying DCU, although
-              the holder can still sell it;
-              <br/>
-              2. the investors cannot open any positions for the future and
-              option contracts, but all the contracts already purchased can
-              still be settled;
-              <br/>
-              3. Probability coin is suspended from buying and selling but
-              the holder can still spend it to win DCU.
-            </Text>
-            <Text textAlign={"end"}>FORT DAO July 6th, 2022</Text>
+      <AlertDialogContent borderRadius={'20px'} bgImage={Banner_URI} bgPosition={"center"} bgSize={"cover"}>
+        <AlertDialogCloseButton
+          top={0}
+          right={'-48px'}
+          color={'white'}
+          borderRadius={'full'}
+          border={'2px'}
+          size={'sm'}
+        />
+        <AlertDialogHeader pt={'40px'} pb={0} fontSize='lg' fontWeight='bold'>
+          <HStack justify={"center"}>
+            <chakra.img src={NEST_URI} h={'20px'}/>
+            <SmallCloseIcon />
+            <chakra.img src={FORT_URI} h={'20px'}/>
+          </HStack>
+        </AlertDialogHeader>
+        <AlertDialogBody p={'40px'}>
+          <Stack fontSize={'14px'} color={'#003434'} fontWeight={'600'} spacing={'44px'} minH={'240px'} justify={"center"}>
+            <Stack spacing={'24px'}>
+              <Text>
+                The technical teams of the FORT protocol and NEST
+                protocol will jointly advance the merger, and the merger is
+                expected to be completed on July 29th.
+              </Text>
+              <Text>
+                Starting from 10:00 (GMT+0) July 7 (some suspensions will
+                take place a few hours earlier due to technical reason).
+              </Text>
+              <Text>
+                1. the users will be suspended from buying DCU, although
+                the holder can still sell it;
+                <br/>
+                2. the investors cannot open any positions for the future and
+                option contracts, but all the contracts already purchased can
+                still be settled;
+                <br/>
+                3. Probability coin is suspended from buying and selling but
+                the holder can still spend it to win DCU.
+              </Text>
+              <Text textAlign={"end"}>FORT DAO July 6th, 2022</Text>
+            </Stack>
           </Stack>
         </AlertDialogBody>
       </AlertDialogContent>
--- src/components/AlertNESTandFORT/index.tsx
+++ src/components/AlertNESTandFORT/index.tsx
@@ -3,7 +3,7 @@ import {
   AlertDialog,
   AlertDialogBody,
   AlertDialogCloseButton,
-  AlertDialogContent,
+  AlertDialogContent, AlertDialogHeader,
   AlertDialogOverlay, chakra, HStack, Stack, Text
 } from "@chakra-ui/react";
 import NEST_URI from "../../assets/svg/nest.svg";
@@ -11,6 +11,7 @@ import FORT_URI from "../../assets/svg/fort.svg";
 import * as React from "react";
 import {atom, useRecoilState} from "recoil";
 import {SmallCloseIcon} from "@chakra-ui/icons";
+import Banner_URI from '../../assets/webp/Ecosystem_pic_1.webp'
 
 const showAlertNESTandFORTAtom = atom({
   key: 'showAlertNESTandFORT',
@@ -30,27 +31,39 @@ const AlertNESTandFORT = () => {
       isCentered
       autoFocus={false}
       closeOnOverlayClick={false}
+      size={'lg'}
     >
       <AlertDialogOverlay/>
-      <AlertDialogContent p={'24px'}>
-        <AlertDialogCloseButton/>
-        <AlertDialogBody>
-          <Stack fontSize={'md'} color={'#003434'} fontWeight={'500'} spacing={'24px'}>
-            <HStack justify={"center"}>
-              <chakra.img src={NEST_URI}/>
-              <SmallCloseIcon />
-              <chakra.img src={FORT_URI}/>
-            </HStack>
-            <Text>
-              The technical teams of the FORT protocol and NEST
-              protocol will jointly advance the merger, and the merger is
-              expected to be completed on July 29th.
-            </Text>
-            <Text>
-              Starting from 10:00 (GMT+0) July 7 (some suspensions will
-              take place a few hours earlier due to technical reason).
-            </Text>
-            <Text textAlign={"end"}>FORT DAO July 6th, 2022</Text>
+      <AlertDialogContent borderRadius={'20px'} bgImage={Banner_URI} bgPosition={"center"} bgSize={"cover"}>
+        <AlertDialogCloseButton
+          top={0}
+          right={'-48px'}
+          color={'white'}
+          borderRadius={'full'}
+          border={'2px'}
+          size={'sm'}
+        />
+        <AlertDialogHeader pt={'40px'} pb={0} fontSize='lg' fontWeight='bold'>
+          <HStack justify={"center"}>
+            <chakra.img src={NEST_URI} h={'20px'}/>
+            <SmallCloseIcon />
+            <chakra.img src={FORT_URI} h={'20px'}/>
+          </HStack>
+        </AlertDialogHeader>
+        <AlertDialogBody p={'40px'}>
+          <Stack fontSize={'14px'} color={'#003434'} fontWeight={'600'} spacing={'44px'} minH={'240px'} justify={"center"}>
+            <Stack spacing={'24px'}>
+              <Text>
+                The technical teams of the FORT protocol and NEST
+                protocol will jointly advance the merger, and the merger is
+                expected to be completed on July 29th.
+              </Text>
+              <Text>
+                Starting from 10:00 (GMT+0) July 7 (some suspensions will
+                take place a few hours earlier due to technical reason).
+              </Text>
+              <Text textAlign={"end"}>FORT DAO July 6th, 2022</Text>
+            </Stack>
           </Stack>
         </AlertDialogBody>
       </AlertDialogContent>
