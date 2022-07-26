84e934b3d4127b3d2353ef4ecfaa3c8a0f590e08
tunogya <tunogya@qq.com>
2022-07-26 16:49:46 +0800

    update

--- src/pages/Research/Info.tsx
+++ src/pages/Research/Info.tsx
@@ -11,50 +11,44 @@ const Info = () => {
       <HStack w={"full"} spacing={'44px'}>
         <HStack color={'#003232'} fontWeight={"semibold"} spacing={'20px'} w={'full'} h={'440px'} bgImage={Research_bg_2}>
           <Spacer/>
-          <Stack pr={'60px'} minW={'320px'}>
-            <Stack
-              fontWeight={'semibold'}
-              color={'#003232'}
-              whiteSpace={'break-spaces'}
-            >
-              <Text fontSize={'25px'}>Promote Industry <br/>Communication</Text>
-              <Text fontSize={'15px'}>NRA’s research activities include but are not
-                <br/>
-                limited to writing research papers published in
-                <br/>
-                peer-reviewed international academic journals,
-                <br/>
-                writing research reports for the public and
-                <br/>
-                investors, and organizing conferences,
-                <br/>
-                workshops, webinars, and AMA.</Text>
-            </Stack>
+          <Stack
+            maxW={'700px'}
+            minW={'300px'}
+            fontWeight={'semibold'}
+            color={'#003232'}
+            whiteSpace={'break-spaces'}
+            pr={'120px'}
+          >
+            <Text fontSize={'25px'}>Promote Industry <br/>Communication</Text>
+            <Text fontSize={'15px'}>NRA’s research activities include but are not
+              <br/>
+              limited to writing research papers published in
+              <br/>
+              peer-reviewed international academic journals,
+              <br/>
+              writing research reports for the public and
+              <br/>
+              investors, and organizing conferences,
+              <br/>
+              workshops, webinars, and AMA.</Text>
           </Stack>
         </HStack>
         <HStack color={'#003232'} fontWeight={"semibold"} spacing={'20px'} w={'full'} h={'440px'} bgImage={Research_bg_3}>
-          <Stack pl={'60px'} minW={'320px'}>
-            <Stack
-              w={'full'}
-              fontWeight={'semibold'}
-              color={'#003232'}
-              whiteSpace={'break-spaces'}
-            >
-              <Text fontSize={'25px'}>Facilitate Academic
-                <br/>
-                Exploration</Text>
-              <Text fontSize={'15px'}>NRA provides funding and job opportunities for
-                <br/>
-                researchers in different stages. NRA and NEST
-                <br/>
-                feel honored if the support helps scholars make
-                <br/>
-                breakthrough progress in academics and achieve
-                <br/>
-                significant contributions to human society from
-                <br/>
-                various perspectives.</Text>
-            </Stack>
+          <Stack pl={'120px'} minW={'320px'}>
+            <Text fontSize={'25px'}>Facilitate Academic
+              <br/>
+              Exploration</Text>
+            <Text fontSize={'15px'}>NRA provides funding and job opportunities for
+              <br/>
+              researchers in different stages. NRA and NEST
+              <br/>
+              feel honored if the support helps scholars make
+              <br/>
+              breakthrough progress in academics and achieve
+              <br/>
+              significant contributions to human society from
+              <br/>
+              various perspectives.</Text>
           </Stack>
           <Spacer/>
         </HStack>
