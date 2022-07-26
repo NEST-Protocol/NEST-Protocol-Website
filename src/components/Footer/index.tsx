6c79ff5f48d230d69a4499cda9ce998ede21d162
tunogya <tunogya@qq.com>
2022-07-26 14:35:44 +0800

    update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { Stack, Text, HStack, useMediaQuery, chakra, Wrap, WrapItem } from '@chakra-ui/react'
+import {Stack, Text, HStack, useMediaQuery, chakra, Wrap, WrapItem, VStack} from '@chakra-ui/react'
 import * as React from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -6,149 +6,55 @@ const Footer = () => {
   const [isDesktop] = useMediaQuery('(min-width: 768px)')
   const navigate = useNavigate()
 
-  if (isDesktop) {
-    return (
-      <Stack
-        px={['24px', '48px']}
-        w={'full'}
-        align={'center'}
-        // bgImage={BG_URL}
-        bgSize={'cover'}
-        bgPosition={"center"}
-        zIndex={'10'}
-      >
-        <Stack
-          position={'absolute'}
-          h={'360px'}
-          w={'full'}
-          bgSize={'contain'}
-          // bgImage={Footer_icon}
-          bgRepeat={'no-repeat'}
-          bgPosition={'center'}
-          zIndex={0}
-        />
-        <Wrap maxW={'1440px'} w={'full'} align={'start'} spacingY={'48px'} pt={'80px'} zIndex={1}>
-          <WrapItem pl={4}>
-            <Stack spacing={'44px'} w={'full'} minW={'180px'}>
-              <Text fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'}>
-                Ecosystem
-              </Text>
-              <Stack
-                spacing={'16px'}
-                color={'#7d7d7d'}
-                fontWeight={'semibold'}
-                fontSize={['12px', '15px']}
-              >
-                <Text
-                  cursor={'pointer'}
-                  onClick={() => {
-                    navigate('ecosystem')
-                    window.scrollTo(0, 0)
-                  }}
-                >
-                  Explore applications
-                </Text>
-              </Stack>
+  return (
+    <Stack
+      px={['24px', '48px']}
+      w={'full'}
+      align={'center'}
+      // bgImage={BG_URL}
+      bgSize={'cover'}
+      bgPosition={"center"}
+      zIndex={'10'}
+    >
+      <Stack maxW={'1440px'} justify={'space-around'} w={'full'}>
+        <Wrap spacing={['22px', '44px']} fontSize={15} w={"full"}>
+          <WrapItem>
+            <Stack spacing={'40px'} minW={'300px'}>
+              <VStack spacing={'22px'} align={"start"}>
+                <Text fontWeight={"bold"} color={'#003232'}>Launch App</Text>
+                <Text color={'#7D7D7D'} fontWeight={'semibold'}>Buy futures, options and win NEST</Text>
+              </VStack>
+              <VStack spacing={'22px'} align={"start"}>
+                <Text fontWeight={"bold"} color={'#003232'}>NEST News</Text>
+                <Text color={'#7D7D7D'} fontWeight={'semibold'}>Latest news from NEST</Text>
+              </VStack>
+              <VStack spacing={'22px'} align={"start"}>
+                <Text fontWeight={"bold"} color={'#003232'}>NEST Research Academy</Text>
+                <Text color={'#7D7D7D'} fontWeight={'semibold'}>Know about NRA</Text>
+              </VStack>
             </Stack>
           </WrapItem>
           <WrapItem>
-            <Stack spacing={'44px'} w={'full'} minW={'180px'}>
-              <Text fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'}>
-                Developers
-              </Text>
-              <Stack
-                spacing={'16px'}
-                color={'#7d7d7d'}
-                fontWeight={'semibold'}
-                fontSize={['12px', '15px']}
-              >
-                <Text
-                  cursor={'pointer'}
-                  onClick={() => {
-                    navigate('docs')
-                    window.scrollTo(0, 0)
-                  }}
-                >
-                  Docs
-                </Text>
-                <Text
-                  cursor={'pointer'}
-                  onClick={() => {
-                    window.open('https://github.com/NEST-Protocol/', '_blank')
-                  }}
-                >
-                  Github
-                </Text>
-                <Text
-                  cursor={'pointer'}
-                  onClick={() => {
-                    navigate('docs/Guide/Set-Up-Local-Environment.md')
-                    window.scrollTo(0, 0)
-                  }}
-                >
-                  Set up local environment
-                </Text>
-              </Stack>
+            <Stack spacing={'40px'} minW={'300px'}>
+              <VStack spacing={'22px'} align={"start"}>
+                <Text fontWeight={"bold"} color={'#003232'}>NEST PVM Docs</Text>
+                <Text color={'#7D7D7D'} fontWeight={'semibold'}>Know how to develop on NEST PVM</Text>
+              </VStack>
+              <VStack spacing={'22px'} align={"start"}>
+                <Text fontWeight={"bold"} color={'#003232'}>NEST Oracle Docs</Text>
+                <Text color={'#7D7D7D'} fontWeight={'semibold'}>Know how to use NEST Oracle</Text>
+              </VStack>
+              <HStack>
+                <Text>Github</Text>
+                <Text>Twitter</Text>
+                <Text>Telegram</Text>
+                <Text>Medium</Text>
+              </HStack>
             </Stack>
           </WrapItem>
           <WrapItem>
-            <Stack spacing={'44px'} w={'full'} minW={'180px'}>
+            <Stack px={'24px'} spacing={'32px'} zIndex={1} w={'500px'}>
               <Text fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'}>
-                Community
-              </Text>
-              <Stack
-                spacing={'16px'}
-                color={'#7d7d7d'}
-                fontWeight={'semibold'}
-                fontSize={['12px', '15px']}
-              >
-                <Text
-                  cursor={'pointer'}
-                  onClick={() => {
-                    navigate('community')
-                    window.scrollTo(0, 0)
-                  }}
-                >
-                  Online communities
-                </Text>
-              </Stack>
-            </Stack>
-          </WrapItem>
-          <WrapItem>
-            <Stack spacing={'44px'} w={'full'} minW={'180px'}>
-              <Text fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'}>
-                Learn
-              </Text>
-              <Stack
-                spacing={'16px'}
-                color={'#7d7d7d'}
-                fontWeight={'semibold'}
-                fontSize={['12px', '15px']}
-              >
-                <Text
-                  cursor={'pointer'}
-                  onClick={() => {
-                    navigate('about')
-                    window.scrollTo(0, 0)
-                  }}
-                >
-                  About NEST
-                </Text>
-                <Text
-                  cursor={'pointer'}
-                  onClick={() => {
-                    navigate('about')
-                    window.scrollTo(0, 0)
-                  }}
-                >
-                  Blogs
-                </Text>
-              </Stack>
-            </Stack>
-          </WrapItem>
-          <WrapItem>
-            <Stack spacing={7} w={'full'} minW={'400px'}>
-              <Text pl={4} fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'}>
                 Market price
               </Text>
               <chakra.div
@@ -156,183 +62,19 @@ const Footer = () => {
                 data-transparent="true"
                 data-design="classic"
                 data-coin-ids="2204"
-                h={'120px'}
+                w={'full'}
               ></chakra.div>
+              <HStack justify={"end"}>
+                <Text px={'24px'} color={'#7D7D7D'} fontSize={'15px'} fontWeight={'semibold'}>
+                  ©2022 NEST
+                </Text>
+              </HStack>
             </Stack>
           </WrapItem>
         </Wrap>
-        <HStack maxW={'1440px'} w={'full'} pr={4} justifyContent={'end'} pb={'80px'} zIndex={1}>
-          <Text color={'#003434'} fontSize={'14px'} fontWeight={'bold'} textAlign={'end'}>
-            ©2022 NEST
-          </Text>
-        </HStack>
       </Stack>
-    )
-  } else {
-    return (
-      <Stack
-        py={'56px'}
-        spacing={'32px'}
-        // bgImage={phone_Footer}
-        bgSize={'cover'}
-        bgPosition={"center"}
-      >
-        <Stack
-          position={'absolute'}
-          h={'360px'}
-          w={'full'}
-          bgSize={'cover'}
-          // bgImage={Footer_icon}
-          bgRepeat={'no-repeat'}
-          bgPosition={'center'}
-          zIndex={0}
-        />
-        <HStack px={'24px'}>
-          <Stack spacing={'32px'} w={'full'} zIndex={1}>
-            <Text fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'}>
-              Ecosystem
-            </Text>
-            <Stack
-              spacing={'16px'}
-              color={'#7d7d7d'}
-              fontWeight={'semibold'}
-              fontSize={['12px', '15px']}
-            >
-              <Text
-                cursor={'pointer'}
-                onClick={() => {
-                  navigate('ecosystem')
-                  window.scrollTo(0, 0)
-                }}
-              >
-                Explore applications
-              </Text>
-            </Stack>
-          </Stack>
-        </HStack>
-        <HStack px={'24px'} align={'start'}>
-          <Stack spacing={'32px'} w={'full'} zIndex={1}>
-            <Text fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'}>
-              Developers
-            </Text>
-            <Stack
-              spacing={'16px'}
-              color={'#7d7d7d'}
-              fontWeight={'semibold'}
-              fontSize={['12px', '15px']}
-            >
-              <Text
-                cursor={'pointer'}
-                onClick={() => {
-                  navigate('docs')
-                  window.scrollTo(0, 0)
-                }}
-              >
-                Docs
-              </Text>
-              <Text
-                cursor={'pointer'}
-                onClick={() => {
-                  window.open('https://github.com/NEST-Protocol', '_blank')
-                }}
-              >
-                Github
-              </Text>
-              <Text
-                cursor={'pointer'}
-                onClick={() => {
-                  navigate('docs/Guide/Set-Up-Local-Environment.md')
-                  window.scrollTo(0, 0)
-                }}
-              >
-                Set up local environment
-              </Text>
-            </Stack>
-          </Stack>
-          <Stack spacing={'32px'} w={'full'} zIndex={1}>
-            <Text fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'}>
-              Community
-            </Text>
-            <Stack
-              spacing={'16px'}
-              color={'#7d7d7d'}
-              fontWeight={'semibold'}
-              fontSize={['12px', '15px']}
-            >
-              <Text
-                cursor={'pointer'}
-                onClick={() => {
-                  navigate('community')
-                  window.scrollTo(0, 0)
-                }}
-              >
-                Online communities
-              </Text>
-            </Stack>
-          </Stack>
-        </HStack>
-        <HStack px={'24px'} align={'start'}>
-          <Stack spacing={'32px'} w={'full'} zIndex={1}>
-            <Text fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'}>
-              Learn
-            </Text>
-            <Stack
-              spacing={'16px'}
-              color={'#7d7d7d'}
-              fontWeight={'semibold'}
-              fontSize={['12px', '15px']}
-            >
-              <Text
-                cursor={'pointer'}
-                onClick={() => {
-                  navigate('about')
-                  window.scrollTo(0, 0)
-                }}
-              >
-                What is NEST
-              </Text>
-            </Stack>
-          </Stack>
-          <Stack spacing={'32px'} w={'full'} zIndex={1}>
-            <Text fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'} opacity={0}>
-              Learn
-            </Text>
-            <Stack
-              spacing={'16px'}
-              color={'#7d7d7d'}
-              fontWeight={'semibold'}
-              fontSize={['12px', '15px']}
-            >
-              <Text
-                cursor={'pointer'}
-                onClick={() => {
-                  navigate('about')
-                  window.scrollTo(0, 0)
-                }}
-              >
-                Blogs
-              </Text>
-            </Stack>
-          </Stack>
-        </HStack>
-        <Stack px={'24px'} spacing={'32px'} zIndex={1}>
-          <Text fontSize={['12px', '15px']} fontWeight={'bold'} color={'#003232'}>
-            Market price
-          </Text>
-          <chakra.div
-            id="crypto-widget-CoinList"
-            data-transparent="true"
-            data-design="classic"
-            data-coin-ids="2204"
-            w={'full'}
-          ></chakra.div>
-        </Stack>
-        <Text px={'24px'} color={'#7D7D7D'} fontSize={'12px'} fontWeight={'semibold'}>
-          ©2022 NEST
-        </Text>
-      </Stack>
-    )
-  }
+    </Stack>
+  )
 }
 
 export default Footer
