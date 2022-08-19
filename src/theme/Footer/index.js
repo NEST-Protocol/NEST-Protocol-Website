f8757fe5a0fb9c7580c2db4aebbb1f3958c47cf2
tunogya <tunogya@qq.com>
2022-08-19 12:18:19 +0800

    update

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import TheFooter from "@site/src/components/TheFooter";
-import {ChakraProvider} from "@chakra-ui/react";
-import theme from "../../chakra";
 
 const Footer = () => {
   return (
-      <ChakraProvider theme={theme}>
-        <TheFooter/>
-      </ChakraProvider>
+  <></>
   )
 }
 
