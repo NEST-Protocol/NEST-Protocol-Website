3b8528ee6425db891d1b7dc584259b44b1f97bab
tunogya <tunogya@qq.com>
2022-08-25 18:15:15 +0800

    update

--- src/chakra/index.js
+++ src/chakra/index.js
@@ -3,11 +3,13 @@ import { extendTheme } from '@chakra-ui/react'
 
 // Component style overrides
 import { Button } from './components/button'
+import {Heading} from "./components/heading";
 
 const overrides = {
   // Other foundational style overrides go here
   components: {
     Button,
+    Heading,
   }
 }
 
