5f666f815278e5d45871afb7eb1998f68c379645
tunogya <tunogya@qq.com>
2022-11-01 15:43:54 +0800

    update

--- src/pages/roundtable/index.tsx
+++ src/pages/roundtable/index.tsx
@@ -134,9 +134,9 @@ const Roundtable = () => {
                           {name} {item.id}
                         </Text>
                         <Spacer/>
-                        { item.pics && item.pics.length > 0 && item.pics.map(item => (
+                        {item.pics && item.pics.length > 0 && item.pics.map(item => (
                           <chakra.img src={item} alt={'icon'}/>
-                          )) }
+                        ))}
                       </HStack>
                       <Text fontWeight={'700'} fontSize={'32px'}>
                         {item.title}
@@ -313,6 +313,117 @@ const Roundtable = () => {
               </ModalContent>
             </Modal>
           </HStack>
+          <Stack align={"center"} pt={'220px'} pb={'280px'}>
+            <Text fontSize={'25px'} fontWeight={'bold'}>NEST Roundtable</Text>
+            <Text fontSize={'12.5px'} fontWeight={'bold'} textAlign={"center"}>NEST Roundtable, A weekly current affairs
+              topic to understand the world of blockchain</Text>
+          </Stack>
+          <Stack spacing={'10px'}>
+            {spaces.slice(0, showMore ? spaces.length : 6).map((item) => (
+              <Stack key={item.link} w={'full'} px={'15px'} overflow={'hidden'}>
+                <Stack w={'full'} bgImage={"/image/roundtable/roundtable-bg.png"} bgSize={'cover'} borderRadius={'10px'}
+                       spacing={'10px'} cursor={"pointer"} p={'10px'}>
+                  <Text fontSize={'12px'} fontWeight={'700'} whiteSpace={'nowrap'}>
+                    {name} {item.id}
+                  </Text>
+                  <HStack>
+                    <Stack spacing={'10px'}>
+                      <Text fontWeight={'700'} fontSize={'16px'}>
+                        {item.title}
+                      </Text>
+                      <HStack alignItems={"center"}>
+                        <chakra.img src={'/svg/data.svg'} w={'12px'}/>
+                        <Text fontSize={'9px'} fontWeight={'500'} color={'#878787'}>
+                          {item.date}
+                        </Text>
+                      </HStack>
+                    </Stack>
+                    <Spacer/>
+                    <Stack minW={'70px'} pr={'20px'} onClick={() => {
+                      window.open(item.link, '_blank')
+                    }}>
+                      <chakra.img src={'/svg/play.svg'} h={'50px'}/>
+                    </Stack>
+                  </HStack>
+                  <HStack spacing={'10px'}>
+                    {item.pics && item.pics.length > 0 && item.pics.map(item => (
+                      <chakra.img src={item} alt={'icon'} h={'10px'}/>
+                    ))}
+                  </HStack>
+                </Stack>
+              </Stack>
+            ))}
+          </Stack>
+          <Stack align={"center"} justify={"center"} pt={'40px'} pb={'60px'} cursor={'pointer'} onClick={() => {
+            setShowMore(!showMore)
+          }}>
+            <Text fontSize={'15px'} fontWeight={'600'}
+                  color={'#00A0E9'}>{showMore ? 'Less' : 'More'} Roundtable <ChevronRightIcon/></Text>
+          </Stack>
+          <Stack py={'44px'} px={'24px'} bg={'rgba(255, 255,255, 0.7)'} justifyContent={"space-between"}
+                 align={"start"}>
+            <Stack spacing={'40px'} align={"start"} w={'full'}>
+              <Stack spacing={'22px'}>
+                <Link fontSize={'15px'} fontWeight={'bold'} href={'https://finance.nestprotocol.org/'} isExternal>Launch
+                  App</Link>
+                <Text fontSize={'15px'} fontWeight={'600'} color={'#7D7D7D'}>Open futures, options NFT here</Text>
+              </Stack>
+              <Stack spacing={'22px'}>
+                <Link fontSize={'15px'} fontWeight={'bold'} href={'/blogs/'}>NEST Blog</Link>
+                <Text fontSize={'15px'} fontWeight={'600'} color={'#7D7D7D'}>Blogs & news from NEST</Text>
+              </Stack>
+              <Stack spacing={'22px'}>
+                <Link fontSize={'15px'} fontWeight={'bold'} href={'/doc/ennestwhitepaper.pdf'}>Whitepaper</Link>
+                <Text fontSize={'15px'} fontWeight={'600'} color={'#7D7D7D'}>Learn more about NEST</Text>
+              </Stack>
+              <Stack spacing={'22px'}>
+                <Link fontSize={'15px'} fontWeight={'bold'} href={'/docs/PVM-Technical-Reference/'}>Developer
+                  Docs</Link>
+                <Text fontSize={'15px'} fontWeight={'600'} color={'#7D7D7D'}>Know how to develop on NEST</Text>
+              </Stack>
+              <Stack spacing={'22px'}>
+                <Link fontSize={'15px'} fontWeight={'bold'} href={'/about/faqs/'}>FAQs</Link>
+                <Text fontSize={'15px'} fontWeight={'600'} color={'#7D7D7D'}>Frequently asked questions</Text>
+              </Stack>
+              <Stack spacing={'10px'} w={'full'}>
+                <Text fontSize={'15px'} fontWeight={'bold'}>Market price</Text>
+                <chakra.div
+                  id="crypto-widget-CoinList"
+                  data-transparent="true"
+                  data-design="classic"
+                  data-coin-ids="2204"
+                  w={'100%'}
+                ></chakra.div>
+              </Stack>
+              <Stack w={'full'} align={"center"} spacing={'12px'}>
+                <HStack spacing={'40px'}>
+                  <Link href={'https://discord.gg/nestprotocol'} isExternal>
+                    <chakra.img src={'/image/Footer/discord_icon.svg'} h={'44px'} w={'44px'} alt={'discord'}/>
+                  </Link>
+                  <Link href={'https://github.com/NEST-Protocol'} isExternal>
+                    <chakra.img src={'/image/Footer/github_icon.svg'} h={'44px'} w={'44px'} alt={'github_icon'}/>
+                  </Link>
+                  <Link href={'https://twitter.com/nest_protocol/'} isExternal>
+                    <chakra.img src={'/image/Footer/twitter_icon.svg'} h={'44px'} w={'44px'} alt={'twitter_icon.svg'}/>
+                  </Link>
+                </HStack>
+                <HStack spacing={'40px'}>
+                  <Link href={'https://t.me/nest_chat/'} isExternal>
+                    <chakra.img src={'/image/Footer/telegram_icon.svg'} h={'44px'} w={'44px'} alt={'telegram_icon'}/>
+                  </Link>
+                  <Link href={'https://nest-protocol.medium.com/'} isExternal>
+                    <chakra.img src={'/image/Footer/medium_icon.svg'} h={'44px'} w={'44px'} alt={'medium'}/>
+                  </Link>
+                  <Link href={'mailto:hello@nestprotocol.org'} isExternal>
+                    <chakra.img src={'/image/Footer/email_icon.svg'} h={'44px'} w={'44px'} alt={'email'}/>
+                  </Link>
+                </HStack>
+              </Stack>
+              <Stack align={"center"} w={'full'}>
+                <Text fontSize={'14px'} textAlign={"center"} fontWeight={'bold'} color={'#7D7D7D'}>@2022 NEST</Text>
+              </Stack>
+            </Stack>
+          </Stack>
         </Stack>
       )}
     </ChakraProvider>
