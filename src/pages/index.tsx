598022cef69e370e1ac1e1d1d0a133bbd274d47b
tunogya <tunogya@qq.com>
2022-11-11 18:37:59 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,10 +119,10 @@ export default function Home(): JSX.Element {
               <Text textAlign={"center"} fontWeight={'600'} fontSize={'21px'}>is a collection of 10,000 NFTs on the blockchain.<br/>
                 What's more, each Cyber Ink can unlock the <br/>NEST Fi membership and additional benefits.</Text>
               <HStack justify={"center"}>
-                <Button w={'160px'} minH={'44px'} fontSize={'25px'} onClick={() => {
+                <Button w={'280px'} minH={'44px'} fontSize={'25px'} onClick={() => {
                   window.open('https://finance.nestprotocol.org/#/NFTAuction', '_blank')
                 }}>
-                  NFT
+                  NFT (BNB Chain)
                 </Button>
               </HStack>
               <chakra.img src={'/image/Home/nft04@2x.png'} cursor={'pointer'} onClick={() => {
